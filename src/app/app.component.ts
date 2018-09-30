687a3b9fcd1c8ee71afacd1278b5d12e4038e4ef
AriellaRogueN <maitilydevidasi@gmail.com>
2018-09-30 12:57:25 -0300

    falta error de tiempo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,83 +8,122 @@ import { Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
   
   title = 'memory-cards';
+
+  orderRandom(){
+    return Math.floor(Math.random() * 8) + 1;
+  }
   cardsList = [
     {
       'dataCard' : '1',
-      'image' : '../assets/img/2.png'
+      'image' : '../assets/img/2.png',
+      'order': this.orderRandom()
     },
     {
       'dataCard': '1',
-      'image': '../assets/img/2.png'
+      'image': '../assets/img/2.png',
+      'order': this.orderRandom()
     },
     {
       'dataCard': '2',
-      'image': '../assets/img/4.png'
+      'image': '../assets/img/4.png',
+      'order': this.orderRandom()
     },
     {
       'dataCard': '2',
-      'image': '../assets/img/4.png'
+      'image': '../assets/img/4.png',
+      'order': this.orderRandom()
     },
     {
       'dataCard': '3',
-      'image': '../assets/img/j.png'
+      'image': '../assets/img/j.png',
+      'order': this.orderRandom()
     },
     {
       'dataCard': '3',
-      'image': '../assets/img/j.png'
+      'image': '../assets/img/j.png',
+      'order': this.orderRandom()
     },
     {
       'dataCard': '4',
-      'image': '../assets/img/k.png'
+      'image': '../assets/img/k.png',
+      'order': this.orderRandom()
     },
     {
       'dataCard': '4',
-      'image': '../assets/img/k.png'
+      'image': '../assets/img/k.png',
+      'order': this.orderRandom()
     }
   ]
+
   hasFlippedCard = false;
   lockBoard = false;
   firstCard: any;
   secondCard: any;
+  countHits =0;
 
-  ngOnInit() { }
-  
+  ngOnInit() {}
+
+  playAudioCard() {
+    let flip = new Audio();
+    flip.src = "../assets/sounds/pop.mp3";
+    flip.load();
+    flip.play();
+  }
+  playAudioWin(){
+    let win = new Audio();
+    win.src = "../assets/sounds/win.mp3";
+    win.load();
+    win.play();
+  }
+
   unflipCards() {
     setTimeout(() => {
       this.firstCard.classList.remove('flip');
       this.secondCard.classList.remove('flip');
       this.resetBoard();
-    }, 1500);
+    }, 800);
   }
+
   resetBoard() {
-  [this.hasFlippedCard, this.lockBoard] = [false, false];
-  [this.firstCard, this.secondCard] = [null, null];
+    [this.hasFlippedCard, this.lockBoard] = [false, false];
+    [this.firstCard, this.secondCard] = [null, null];
   }  
 
   disableCards() {
+    this.countHits++
+    if (this.countHits == 4) this.playAudioWin()
     this.firstCard.removeEventListener('click', this.flipCard);
     this.secondCard.removeEventListener('click', this.flipCard);
+    this.firstCard.classList.add('hidden');
+    this.secondCard.classList.add('hidden');
     this.resetBoard();
   }
+
   checkForMatch() {
+    console.log(this.firstCard.dataset)
     let isMatch = this.firstCard.dataset.card === this.secondCard.dataset.card;
     isMatch ? this.disableCards() : this.unflipCards();
   }
 
   flipCard(event) {
-    let node = event.target;
-    event.target.parentNode.classList.add('flip');
+    let node = event.target.parentNode;
+    node.classList.add('flip');
+    this.playAudioCard();
 
     if (this.lockBoard) return;
     if (node === this.firstCard) return;
 
     if (!this.hasFlippedCard) {
-    this.hasFlippedCard = true;
-      this.firstCard = this;
+      this.hasFlippedCard = true;
+      this.firstCard = node;
       return;
     }
     this.secondCard = node;
     this.lockBoard = true;
     this.checkForMatch(); 
+  }
+  reload(){
+    console.log('kj')
+    window.location.reload();
   }
 }
