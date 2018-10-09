459a77111a6c936ed0e5ede97eb23ea3d0d9901f
AriellaRogueN <maitilydevidasi@gmail.com>
2018-10-09 08:20:09 -0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:
+    {
+      apiKey: "AIzaSyBIiNv1rf-tE66PjkDIOUCKVUYwJIw2d8I",
+      authDomain: "memory-cards-32f58.firebaseapp.com",
+      databaseURL: "https://memory-cards-32f58.firebaseio.com",
+      projectId: "memory-cards-32f58",
+      storageBucket: "memory-cards-32f58.appspot.com",
+      messagingSenderId: "608567712276"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:
+  {
+    apiKey: "AIzaSyBIiNv1rf-tE66PjkDIOUCKVUYwJIw2d8I",
+    authDomain: "memory-cards-32f58.firebaseapp.com",
+    databaseURL: "https://memory-cards-32f58.firebaseio.com",
+    projectId: "memory-cards-32f58",
+    storageBucket: "memory-cards-32f58.appspot.com",
+    messagingSenderId: "608567712276"
+  }
 };
 
 /*
